25303f83f9a727d43501c82531c9f8fdb73a5976
nick747 <micco.mario@gmail.com>
2023-07-29 09:33:58 +0200

    Loaded html and css

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="content">
+    <div class="colorContainer"  id="color1">
+      <h1 class="colorHex" id="color1Hex"></h1>
+    </div>
+    <div class="colorContainer" id="color2">
+      <h1 class="colorHex" id="color2Hex"></h1>
+    </div>
+    <div class="colorContainer" id="color3">
+      <h1 class="colorHex" id="color3Hex"></h1>
+    </div>
+    <div class="colorContainer" id="color4">
+      <h1 class="colorHex" id="color4Hex"></h1>
+    </div>
   </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  <div>
+    <button onclick="button()">GENERATE RANDOM PALETTE</button>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
